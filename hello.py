ac11406be6fa00f43cb464cbf7b9a773cb3d8fe2
codeAKstan <boystanismoses@gmail.com>
2024-07-14 17:26:23 +0100

    data structures practice

--- hello.py
+++ hello.py
@@ -1,7 +1,25 @@
-import math
-from utilities import number_utils
-# using standard module
-print(f"standard: {math.factorial(3)}")
+# list
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# using my defined module and package
-print(f"defined: {number_utils.fact(3)}")
+myList.append(11)
+myList.remove(5)
+print(myList)
+# tuple
+my_tuple = ("ghana", "usa", "england", "canada", "nigeria")
+third_tuple = my_tuple[2]
+print(third_tuple)
+# set
+my_set = {1, 2, 3, 4, 5}
+my_set.add(6)
+my_set.remove(2)
+print(my_set)
+# dictionary
+my_dict = {
+    'apple': 'red',
+    'banana': 'yellow',
+    'pawpaw': 'green'
+}
+my_dict['grape'] = 'purple'
+del my_dict['banana']
+
+print(my_dict)
