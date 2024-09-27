ef9d36e62cf7b537072937e57bdb3074ed5c167b
codeAKstan <boystanismoses@gmail.com>
2024-09-27 10:10:33 +0100

    Submitting a post to JSONPlaceholder API using POST method

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
 
-url = 'https://jsonplaceholder.typicode.com/users'
+url = 'https://jsonplaceholder.typicode.com/posts'
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    users = response.json()
-    for user in users:
-        print(f'name: {user["name"]}')
-        print(f"email: {user['email']}")
+new_post = {
+    'title': "project",
+    'body': "this is my project body",
+    'userId': 2
+}
+response = requests.post(url, json=new_post)
+
+if response.status_code == 201:
+    print("this is a new post")
+    print(response.json())
 
 else:
-    print("error")
+    print("failed")
