276a6343b6e2963e50693566901d4cc753ac8e4c
codeAKstan <boystanismoses@gmail.com>
2024-09-27 12:03:08 +0100

    lsit comprehension, numbers divisible by 3

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-def fib(n):
-    a, b = 0, 1
-    fib_seq = [a, b]
-
-    while len(fib_seq) < n:
-        next = a + b
-        fib_seq.append(next)
-        a, b = b, next
-
-    return fib_seq
-
-
-print(fib(10))
+divThree = [x for x in range(1, 21) if x % 3 == 0]
+print(divThree)
