1be13429bce79a068889f899af910ed41d28422f
codeAKstan <boystanismoses@gmail.com>
2024-10-19 14:44:26 +0100

    Odd or even checker

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for x in range(1, 11):
-    print(x)
+num = int(input("Enter a no: "))
+
+if num > 0:
+    if num % 2 == 0:
+        print("even number")
+    else:
+        print("Odd no")
+else:
+    print("you entered a negative no")
