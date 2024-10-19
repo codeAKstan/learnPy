15e024c6324dcd606c7c16d246d952a7d490a2d3
codeAKstan <boystanismoses@gmail.com>
2024-10-19 14:53:33 +0100

    sum of numbers up to n

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 num = int(input("Enter a no: "))
-
-if num > 0:
-    if num % 2 == 0:
-        print("even number")
-    else:
-        print("Odd no")
-else:
-    print("you entered a negative no")
+count = 0
+sum = 0
+while count < num:
+    count += 1
+    sum += count
+print(sum)
