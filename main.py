28726770b0118d7ed45ce2b38d71180799af72d1
codeAKstan <boystanismoses@gmail.com>
2024-10-19 14:07:44 +0100

    Variable swapping without third variable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-name = input("what's your name? ")
-age = int(input("your age? "))
-print(f'Hello, {name}! you are {age} years old')
+fvar = 3
+svar = 6
+
+fvar, svar = svar, fvar
+
+print(f'fvar: {fvar}, svar:{svar}')
