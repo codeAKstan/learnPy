90454982ce667f9547f2e9842bdf12e945934797
codeAKstan <boystanismoses@gmail.com>
2024-10-20 13:10:51 +0100

    keyword and positional arguments

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b=8):
-    return a + b
+def display_info(name, age):
+    print(f"Name: {name}, Age: {age}")
 
-print(add_numbers(6))
+# Positional arguments
+display_info("Obumneme", 24)
+
+# Keyword arguments
+display_info(age=24, name="Obumneme")
