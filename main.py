aea39a88e97974bab429b22b84a6691391c63c36
codeAKstan <boystanismoses@gmail.com>
2024-10-20 14:52:35 +0100

    added a simple module

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def display_info(name, age):
-    print(f"Name: {name}, Age: {age}")
+from my_module import greet
 
-# Positional arguments
-display_info("Obumneme", 24)
-
-# Keyword arguments
-display_info(age=24, name="Obumneme")
+greet("codeakstan")
