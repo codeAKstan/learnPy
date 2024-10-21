5f844f8333530c9c6c1e2fd2cd71e9d51e60b8ca
codeAKstan <boystanismoses@gmail.com>
2024-10-21 15:10:30 +0100

    Factorial calculation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from my_module import fahrenheit_to_celsius
+from my_module import fahrenheit_to_celsius, factorial
 
-temp_f = float(input("Enter a temp in fahrenheit: "))
-temp_c = fahrenheit_to_celsius(temp_f)
-print(f'{temp_f} F is {temp_c} C')
+# temp_f = float(input("Enter a temp in fahrenheit: "))
+# temp_c = fahrenheit_to_celsius(temp_f)
+# print(f'{temp_f} F is {temp_c} C')
+
+num = int(input("Enter a no to calc its factorial: "))
+print(factorial(num))
--- my_module.py
+++ my_module.py
@@ -1,3 +1,10 @@
 def fahrenheit_to_celsius(f):
     c = (f - 32) * (5/9)
     return c
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        result = n * factorial(n -1)
+        return  result
