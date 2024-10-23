2c52ddba1530452387f7bb3e94e6b83b3af1cfae
codeAKstan <boystanismoses@gmail.com>
2024-10-23 08:08:45 +0100

    A simple bankaccount class

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+class BankAccount:
+    def __init__(self, account_balance=1000):
+        self.__account_balance = account_balance
 
-    def area(self):
-        return self.width * self.height
-    def perimeter(self):
-        return 2 * self.width * self.height
+    def deposit(self, amount):
+        if amount <= 0:
+            return "Amount must be greater than 0"
+        self.__account_balance += amount
+        return f'deposited {amount}'
 
-rect = Rectangle(5, 8)
-print(rect.area())
-print(rect.perimeter())
+    def withdraw(self, amount):
+        if amount <= 0:
+            return "amount must be greater than 0"
+        if amount > self.__account_balance:
+            return "Insufficient balance"
+        self.__account_balance -= amount
+        return f'withdrew {amount}'
+
+    def check_balance(self):
+        return f'Your account balance is {self.__account_balance}'
+
+bank = BankAccount()
+dep_amt = float(input("Enter the amount you want to deposit "))
+with_amt = float(input("Enter the amount to withdraw "))
+print(bank.deposit(dep_amt))
+print(bank.withdraw(with_amt))
+print(bank.check_balance())
