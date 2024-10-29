3f7a59697036b4e027804d84c369697e980daae7
codeAKstan <boystanismoses@gmail.com>
2024-10-29 15:01:42 +0100

    A simple uniitest

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-squares = (x**2 for x in range(5))
-print(list(squares))
+import  unittest
+
+def add(a, b):
+    return a + b
+
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -2), -4)
+
+
+if __name__ == "__main__":
+    unittest.main()
